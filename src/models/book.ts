4e7f0598f1d13680cd1757983020309fe3e1d459
Caio Simões <stampedesw29@gmail.com>
2023-12-01 16:48:14 -0300

    created models for the api

---
+++ src/models/book.ts
@@ -0,0 +1,41 @@
+import { DataTypes } from "sequelize";
+import database from "./db.js";
+
+const Book = database.define(
+  "Book",
+  {
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      unique: true,
+    },
+    title: {
+      type: DataTypes.STRING(150),
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    pages: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+      validate: {
+        min: 0,
+        max: 10,
+      },
+    },
+  },
+  {
+    schema: "public",
+  }
+);
+
+export default Book;
---
+++ src/models/users.ts
@@ -0,0 +1,39 @@
+import { DataTypes } from "sequelize";
+import database from "./db.js";
+
+// Postgres is not case sensitive, but mysql is (in linux);
+// Primary Key = It's unique and the one who's going to identify the table
+// (Which in this case, is the user)
+
+const User = database.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      unique: true,
+    },
+    usename: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    userEmail: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    bookshelf: {
+      type: DataTypes.ARRAY,
+    },
+    favourites: {
+      type: DataTypes.ARRAY,
+    },
+  },
+  {
+    schema: "public",
+  }
+);
