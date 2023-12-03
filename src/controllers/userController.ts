b7113b5ca6f3a642213065a92c0a7e5387fe9b6b
Caio Simões <stampedesw29@gmail.com>
2023-12-03 14:46:51 -0300

    testing: a class for the user object

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,11 +1,36 @@
 import { Request, Response } from "express";
 import { randomUUID } from "crypto";
 
-type User = {
+interface UserInterface {
   id: string;
   username: string;
-  passowrd: string;
+  password: string;
   userEmail: string;
   bookshelf: Array<any>;
   favourites: Array<any>;
-};
+}
+
+class User implements UserInterface {
+  id: string;
+  username: string;
+  password: string;
+  userEmail: string;
+  bookshelf: any[];
+  favourites: any[];
+  constructor(username: string, userEmail: string, password: string) {
+    this.id = randomUUID();
+    this.username = username;
+    this.userEmail = userEmail;
+    this.password = password;
+    this.bookshelf = [];
+    this.favourites = [];
+  }
+
+  register() {
+
+  }
+
+  login() {
+
+  }
+}
